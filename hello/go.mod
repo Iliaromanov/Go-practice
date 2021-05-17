4b3c07345fba6ad10b3552d31e71a4dcb1f10e97
Iliaromanov <iliaromanov033@gmail.com>
2021-05-16 21:04:46 -0400

    importing modules

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,5 @@
 module github.com/hello
 
 go 1.16
+
+require rsc.io/quote v1.5.2
--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+import "rsc.io/quote"
+
 func main() {
 	fmt.Println("Hello, World! (in Go)")
+	fmt.Println(quote.Go())
 }
