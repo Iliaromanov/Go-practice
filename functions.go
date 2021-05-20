1bf9f559b922925ed00a60558cd771584f31a86e
Iliaromanov <iliaromanov033@gmail.com>
2021-05-19 22:12:28 -0400

    two ways of declaring variables

--- functions.go
+++ functions.go
@@ -20,10 +20,20 @@ func isOdd(num int) bool {
 		
 }
 
+func makeBox(width int, height int) string {
+	var box string;
+	box = fmt.Sprintf("This is a %v by %v box.", width, height)
+	// Two lines above equivalent to "box := fmt.Sprintf("This is a %v by %v box.", width, height)"
+	// := determines what type the variable is based on the value thats being assigned
+
+	return box
+}
+
 func main() {
 	fmt.Println("This is the start of the program!")
 	countFromTo(0, 5)
 	sevenOdd := isOdd(7) // Assigning to variable
 	fmt.Printf("7 is odd? - %t\n", sevenOdd) // Can pass variable
 	fmt.Printf("8 is odd? - %t\n", isOdd(8)) // Can pass func result directly
+	fmt.Println(makeBox(4, 5))
 }
