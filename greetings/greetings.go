959ba22028fa7a46d15889fd46b41350a3912858
Iliaromanov <iliaromanov033@gmail.com>
2021-05-20 21:13:28 -0400

    returning and handling errors using 'log' and 'error' modules

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,22 +1,39 @@
 package greetings
 
-import "fmt"
+import (
+	"fmt"
+	"errors"
+)
 
 /*
 *** In Go, a function whose name starts with a capital letter can be called by a function not in the same package. ***
 *** This is known as an exported name. ***
 */
 
-// Hello returns a greeting for the named person.
-func HelloFromal(firstName string, lastName string) string { 
-	// Return a greeting that embeds the name in a message.
-	msg := fmt.Sprintf("Hello, %v %v. Welcome!", firstName, lastName) // Create a formatted string
-	return msg
+func checkEmpty(name string) bool {
+	return name == ""
 }
 
-func HelloInformal(name string) string {
+// Hello returns a greeting for the named person.
+func HelloFromal(firstName string, lastName string) (string, error) {
+
+	if checkEmpty(firstName) || checkEmpty(lastName) {
+		return "", errors.New("empty name field")
+	}
+
+	// Return a greeting that embeds the name in a message.
+	msg := fmt.Sprintf("Hello, %v %v. Welcome!", firstName, lastName) // Create a formatted string
+
+	return msg, nil
+}
+
+func HelloInformal(name string) (string, error) {
+
+	if checkEmpty(name) {
+		return "", errors.New("empty name field")
+	}
 	var msg string
 	msg = fmt.Sprintf("Hey %v!", name)
 	msg += " What's up???"
-	return msg
+	return msg, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"example.com/greetings"
 )
 import "rsc.io/quote"
 
 func main() {
-	messageFormal := greetings.HelloFromal("Ilia", "Romanov")
+	log.SetPrefix("greetings: ") // Prints this before any other log messages
+	log.SetFlags(0)
+
+	messageFormal, err1 := greetings.HelloFromal("Ilia", "")
+	// If error returned print it to console and exit program
+	if err1 != nil {
+		log.Fatal(err1) // .Fatal prints given argument and stops the program.
+	}
+
 	fmt.Println(messageFormal)
-	messageInformal := greetings.HelloInformal("Ilia")
+	messageInformal, err2 := greetings.HelloInformal("Ilia")
+	// If error returned print it to console and exit program
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
 	fmt.Println(messageInformal)
 	fmt.Println(quote.Go())
 }
