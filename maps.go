9692b39b4056bb4091b8b3916ce979c5d8f6cdd4
Iliaromanov <iliaromanov033@gmail.com>
2021-05-22 17:37:38 -0400

    printMaps() func

--- maps.go
+++ maps.go
@@ -2,19 +2,30 @@ package main
 
 import "fmt"
 
+func printMap(m map[string]interface{}) {
+	for key := range m {
+		fmt.Printf("'%v': %v\n", key, m[key])
+	}
+}
+
 func main() {
-	balances := make(map[string]float32)
+	balances := make(map[string]interface{})
 	balances["Bob"] = 0.25
 	balances["Alice"] = 5.50
 	balances["Bartholamew"] = 1500.11
-	fmt.Print(balances) // >> map[Alice:5.5 Bartholamew:1500.11 Bob:0.25]
+	printMap(balances) // >> map[Alice:5.5 Bartholamew:1500.11 Bob:0.25]
 
-	var langs = map[string]string{
+	var langs = map[string]interface{} {
 		"Python": "$$$$$",
 		"Go": "$$$$$",
 		"C": "$$$",
 		"SQL": "$",
-		"JavaScript": "$$",	
 	}
-	fmt.Print(langs)// >> map[C:$$$ Go:$$$$$ JavaScript:$$ Python:$$$$$ SQL:$]
+	langs["JavaScript"] = "$$"
+	val, ok := langs["SQL"]
+	fmt.Printf("val: %v, ok: %t\n", val, ok) // >> val: $, ok: true
+	delete(langs, "SQL") // use 'delete' to remove key-value pairs
+	val, ok = langs["SQL"]
+	fmt.Printf("val: %v, ok: %t\n", val, ok) // >> val: , ok: false
+	printMap(langs)// >> map[C:$$$ Go:$$$$$ JavaScript:$$ Python:$$$$$]
 }
