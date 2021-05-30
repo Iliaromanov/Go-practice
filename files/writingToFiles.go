f05f47063ddfbbc982859fa66ca913a565d9ff62
Iliaromanov <iliaromanov033@gmail.com>
2021-05-30 15:08:52 -0400

    appending a string to end of file

--- files/writingToFiles.go
+++ files/writingToFiles.go
@@ -21,10 +21,18 @@ func main() {
 
 	writeFilePath := filepath.Join(cwd, "writeToThis.txt")
 	// Write array of bytes (string) to file
-	msg1 := []byte("This is first message!")
-	msg2 := []byte("This is second message!!")
+	msg1 := []byte("This is first message!\n")
+	msg2 := []byte("This is second message!!\n")
 	err = ioutil.WriteFile(writeFilePath, msg1, 0644) // 0644 allows user to read and write, all others only read
 	checkError(err)
 	err = ioutil.WriteFile(writeFilePath, msg2, 0644) // msg2 overwrites msg1
+	checkError(err)
+
+	// Open and append to file (or create it if doesn't already exist)
+	f, err := os.OpenFile(writeFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	checkError(err)
+	defer f.Close()
+
+	_, err = f.WriteString("This is third message!!!\n")
 	checkError(err)
 }
