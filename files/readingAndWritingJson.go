a90699c3c2eee9be3e6bd0e1817f85eca26380e4
Iliaromanov <iliaromanov033@gmail.com>
2021-05-31 15:10:28 -0400

    reading json without structs (not recommended)

--- files/readingAndWritingJson.go
+++ files/readingAndWritingJson.go
@@ -29,12 +29,26 @@ func main() {
 	// unmarshal json data into structs defined above
 	var stocks Stocks
 	json.Unmarshal(byteValue, &stocks)
-	println("here:", stocks.Stocks)
 
 	for _, stock := range(stocks.Stocks) {
 		fmt.Printf("~~~\nname: %v | value: %v | lastUpdated: %v\n~~~\n", 
 			stock.Name, stock.ShareValue, stock.LastUpdate)
 	}
+	f.Close()
+
+	// Reopen file
+	f, err = os.Open("stockInfo.json")
+	checkError(err)
+	defer f.Close()
+
+	// Reading JSON without using structs (using interface{} instead)
+	byteValue2, err := ioutil.ReadAll(f)
+	
+	checkError(err)
+	var result map[string]interface{}
+	json.Unmarshal([]byte(byteValue2), &result)
+	fmt.Println(result["stocks"])
+
 }
 
 
