617b5e080f4707c0bf8c41aa4e0c5ff109fb0b55
Iliaromanov <iliaromanov033@gmail.com>
2021-06-12 19:00:34 -0400

    local and global flags, commands with arguments

--- cobra-CLI-Library/main.go
+++ cobra-CLI-Library/main.go
@@ -3,22 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
-
+	"strings"
 	"github.com/spf13/cobra"
 )
 
 var (
+	localRootFlag bool // Only available for rootCmd
+	persistRootFlag bool // Available for all commands
+
 	rootCmd = &cobra.Command{
 		Use: "example",
 		Short: "An example cobra program",
+	}
+
+	echoCmd = &cobra.Command{
+		Use: "echo [strings to echo]",
+		Short: "prints given strings to stdout",
+		Args: cobra.MinimumNArgs(1), // Must give at least one argument
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Hello from root command")
+			fmt.Println("Echo: " + strings.Join(args, " "))
 		},
 	}
 )
 
 func init() {
-	
+	rootCmd.PersistentFlags().BoolVarP(&persistRootFlag, "persistFlag", "p", false, "a persistent root flag")
+	rootCmd.Flags().BoolVarP(&localRootFlag, "localFlag", "l", false, "a local root flag")
+	rootCmd.AddCommand(echoCmd)
 }
 
 func main() {
