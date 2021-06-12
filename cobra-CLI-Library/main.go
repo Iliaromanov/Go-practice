62677ad4c6f152b5fc45c6e669d958bb7305e903
Iliaromanov <iliaromanov033@gmail.com>
2021-06-12 19:58:35 -0400

    sub-commands for non-root commands

--- cobra-CLI-Library/main.go
+++ cobra-CLI-Library/main.go
@@ -10,7 +10,7 @@ import (
 var (
 	localRootFlag bool // Only available for rootCmd
 	persistRootFlag bool // Available for all commands
-
+	times int
 	rootCmd = &cobra.Command{
 		Use: "example",
 		Short: "An example cobra program",
@@ -24,12 +24,24 @@ var (
 			fmt.Println("Echo: " + strings.Join(args, " "))
 		},
 	}
+	timesCmd = &cobra.Command{
+		Use: "times [strings to echo]",
+		Short: "prints given strings multiple times",
+		Args: cobra.MaximumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			for i := 0; i < times; i++ {
+				fmt.Println("Echo:", strings.Join(args, " "))
+			}
+		},
+	}
 )
 
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&persistRootFlag, "persistFlag", "p", false, "a persistent root flag")
 	rootCmd.Flags().BoolVarP(&localRootFlag, "localFlag", "l", false, "a local root flag")
+	timesCmd.Flags().IntVarP(&times, "times", "t", 1, "number of times to echo to stdout")
 	rootCmd.AddCommand(echoCmd)
+	echoCmd.AddCommand(timesCmd)
 }
 
 func main() {
